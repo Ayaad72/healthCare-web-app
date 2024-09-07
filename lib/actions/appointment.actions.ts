0394a9fd4819f121e45db0331ddb8acfadfb480e
ayaad <ayaadalavi@gmail.com>
2024-09-07 11:09:51 +0000

    first commit

---
+++ lib/actions/appointment.actions.ts
@@ -0,0 +1,157 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { ID, Query } from "node-appwrite";
+
+import { Appointment } from "@/types/appwrite.types";
+
+import {
+  APPOINTMENT_COLLECTION_ID,
+  DATABASE_ID,
+  databases,
+  messaging,
+} from "../appwrite.config";
+import { formatDateTime, parseStringify } from "../utils";
+
+// CREATE APPOINTMENT
+export const createAppointment = async (
+  appointment: CreateAppointmentParams
+) => {
+  try {
+    // Ensure the appointment object has valid structure
+    const { userId, schedule, primaryPhysician, ...rest } = appointment;
+
+    const newAppointment = await databases.createDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        userId, // Ensure this field exists in Appwrite schema
+        schedule,
+        primaryPhysician,
+        ...rest,
+      }
+    );
+
+    revalidatePath("/admin");
+    return parseStringify(newAppointment);
+  } catch (error) {
+    console.error("An error occurred while creating a new appointment:", error);
+  }
+};
+
+// GET RECENT APPOINTMENTS
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")]
+    );
+
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        switch (appointment.status) {
+          case "scheduled":
+            acc.scheduledCount++;
+            break;
+          case "pending":
+            acc.pendingCount++;
+            break;
+          case "cancelled":
+            acc.cancelledCount++;
+            break;
+        }
+        return acc;
+      },
+      initialCounts
+    );
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
+
+    return parseStringify(data);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the recent appointments:",
+      error
+    );
+  }
+};
+
+// SEND SMS NOTIFICATION
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+    return parseStringify(message);
+  } catch (error) {
+    console.error("An error occurred while sending SMS:", error);
+  }
+};
+
+// UPDATE APPOINTMENT
+export const updateAppointment = async ({
+  appointmentId,
+  userId,
+  appointment,
+  type,
+}: UpdateAppointmentParams) => {
+  try {
+    const updatedAppointment = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
+
+    if (!updatedAppointment) throw Error;
+
+    const smsMessage = `Greetings from CarePulse. ${
+      type === "schedule"
+        ? `Your appointment is confirmed for ${
+            formatDateTime(appointment.schedule).dateTime
+          } with Dr. ${appointment.primaryPhysician}`
+        : `We regret to inform you that your appointment for ${
+            formatDateTime(appointment.schedule).dateTime
+          } is cancelled. Reason:  ${appointment.cancellationReason}`
+    }.`;
+    await sendSMSNotification(userId, smsMessage);
+
+    revalidatePath("/admin");
+    return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.error("An error occurred while scheduling an appointment:", error);
+  }
+};
+
+// GET APPOINTMENT
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the existing patient:",
+      error
+    );
+  }
+};
